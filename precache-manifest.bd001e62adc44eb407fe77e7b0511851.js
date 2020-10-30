55069d5cbd07ae47e77adc9314a3b9de2ee0b715
etteryand0 <etteryand@gmail.com>
2020-10-30 12:25:55 +0900

    Updates

---
+++ precache-manifest.bd001e62adc44eb407fe77e7b0511851.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6819d195e826d48c32bac6a8489120c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d00cbd7d4f9e8bf67078",
+    "url": "/static/css/2.6326594c.chunk.css"
+  },
+  {
+    "revision": "cd97cab9bc80e56988be",
+    "url": "/static/css/main.439b4a20.chunk.css"
+  },
+  {
+    "revision": "d00cbd7d4f9e8bf67078",
+    "url": "/static/js/2.5647f116.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.5647f116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd97cab9bc80e56988be",
+    "url": "/static/js/main.6d7aa988.chunk.js"
+  },
+  {
+    "revision": "6bafc3225efe5902e923",
+    "url": "/static/js/runtime-main.ca63edf2.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
